66fd4042505e7d127483be58201e648e9d45a5c3
Dhanashree Trambadiya <dhanashreetrambadiya@gmail.com>
2025-08-19 15:05:52 +0530

    just simple step to convert money

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <h2> Currency Converter</h2>
+
+     <input
+     type = "number"
+     placeholder='Enter amounttt plz'
+     style = {{marginRight : "10px", padding : "6px"}}>
+
+     </input>
+
+    <select style={{marginRight : "10px", padding : "6px"}}>
+    <option value= "USD">USD - US dollar</option>
+        <option value= "RUB">RUB - Russian ruble</option>
+
+    <option value= "EUR">EUR - Euro</option>
+
+    <option value= "INR">INR - Indian rupee</option>
+
+    <option value= "GBP">GBP - British pound </option>
+    <option value= "JPY">JPY - Japanese Yen</option>
+    <option value= "AUD">AUD - Australian dollar</option>
+    <option value= "CAD">CAD - Canadian dollar</option>
+    <option value= "CHF">CHF - SWiss franc</option>
+    <option value= "CNY">CNY - Chinese YUan</option>
+
+    </select>
+
+
+
+
+    <select style={{marginRight : "10px", padding : "6px"}}>
+    <option value= "USD">USD - US dollar</option>
+        <option value= "RUB">RUB - Russian ruble</option>
+
+    <option value= "EUR">EUR - Euro</option>
+
+    <option value= "INR">INR - Indian rupee</option>
+
+    <option value= "GBP">GBP - British pound </option>
+    <option value= "JPY">JPY - Japanese Yen</option>
+    <option value= "AUD">AUD - Australian dollar</option>
+    <option value= "CAD">CAD - Canadian dollar</option>
+    <option value= "CHF">CHF - SWiss franc</option>
+    <option value= "CNY">CNY - Chinese YUan</option>
+
+    </select>
+
+    
+{/* 
+     <input type="number"></input> */}
+     <button>Convert</button>
+
+
+
     </div>
   );
 }
